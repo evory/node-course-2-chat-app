53a123fe7565fce0d222f065accc12512648bcc5
Bryan <bryanbrandt67@gmail.com>
2018-03-26 00:46:58 +0200

    adding time to messages

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+const moment = require ('moment');
+
+// var date = moment();
+
+// date.add(100, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var someTimestamp = moment().valueOf();
+console.log(smneTimestamp);
+var createdAt = 1234;
+var date = moment();
+console.log(date.format('h:mm a'));
